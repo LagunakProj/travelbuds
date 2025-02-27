357b0739704b6461f8e43ec82123609cc189970e
Harriet Landa Gil <hlandagil@gmail.com>
2025-02-27 09:05:15 +0100

    Add authentication pages and update user sign-up action

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+	return (
+		<div>
+			<h1 className="text-6xl font-medium">About</h1>
+		</div>
+	)
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,9 @@ import { ThemeProvider } from "next-themes"
 import Link from "next/link"
 import Image from "next/image"
 import "./globals.css"
-import {
-	ClerkProvider,
-	SignInButton,
-	SignUpButton,
-	SignedIn,
-	SignedOut,
-	UserButton,
-} from "@clerk/nextjs"
+import { hasEnvVars } from "@/utils/supabase/check-env-vars"
+import { EnvVarWarning } from "@/components/env-var-warning"
+import HeaderAuth from "@/components/header-auth"
 
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
@@ -34,47 +29,45 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<ClerkProvider>
-			<html lang="en" className={geistSans.className} suppressHydrationWarning>
-				<body className="bg-background text-foreground">
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange
-					>
-						<header className="px-4 lg:px-6 h-14 flex items-center align-center w-full border-b">
-							<Link className="flex items-center justify-center" href="#">
-								<Image src="/icon.webp" alt="TravelBuds" width={30} height={30} />
-								<span className="font-bold">TravelBuds</span>
+		<html lang="en" className={geistSans.className} suppressHydrationWarning>
+			<body className="bg-background text-foreground">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<header className="px-4 lg:px-6 h-14 flex items-center align-center w-full border-b">
+						<Link className="flex items-center justify-center" href="/">
+							<Image src="/icon.webp" alt="TravelBuds" width={30} height={30} />
+							<span className="font-bold">TravelBuds</span>
+						</Link>
+						<nav className="ml-auto flex gap-6 mr-4">
+							<Link
+								className="text-sm font-medium hover:underline underline-offset-4"
+								href="/budgroups"
+							>
+								My BudGroups
 							</Link>
-							<nav className="ml-auto flex gap-6 mr-4">
-								<Link
-									className="text-sm font-medium hover:underline underline-offset-4"
-									href="/budgroups"
-								>
-									My BudGroups
-								</Link>
-								<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-									About Us
-								</Link>
-							</nav>
-							<SignedOut>
-								<SignInButton />
-								<SignUpButton />
-							</SignedOut>
-							<SignedIn>
-								<UserButton />
-							</SignedIn>
-						</header>
-						<main className="min-h-screen flex flex-col items-center">
-							<div className="flex-1 w-full flex flex-col gap-20 items-center">
-								<div className="flex flex-col gap-20 w-full ">{children}</div>
+							<Link
+								className="text-sm font-medium hover:underline underline-offset-4"
+								href="/about"
+							>
+								About Us
+							</Link>
+						</nav>
+						{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+						<ThemeSwitcher />
+					</header>
+					<main className="min-h-screen flex flex-col items-center">
+						<div className="flex-1 w-full flex flex-col gap-20 items-center">
+							<div className="flex flex-col gap-20 w-full justify-center items-center justify-center">
+								{children}
 							</div>
-						</main>
-					</ThemeProvider>
-				</body>
-			</html>
-		</ClerkProvider>
+						</div>
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
 	)
 }
